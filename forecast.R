549f8c3378c30c13bbe360ea012a720912ada631
parsa zendehdel <62348090+parsazn@users.noreply.github.com>
2024-09-09 15:10:27 +0200

    Script modified

--- forecast.R
+++ forecast.R
@@ -7,24 +7,24 @@ library(lubridate)
 data <- read.csv("/Users/parsazn/Documents/TFG/Data-Collection-Services/EDMIDC-forecast-service/results.csv", comment.char = "#", skip = 3)
 
 # Convert _time to POSIXct format to handle date and time
-data$createdDate <- as.POSIXct(data$`X_time`, format="%Y-%m-%dT%H:%M:%OSZ")
+data$createdDate <- as.POSIXct(data$`X_time`, format="%Y-%m-%dT%H:%M:%OSZ", tz="UTC")
 
 # Ensure that the data is sorted by date and time
 data <- data %>% arrange(createdDate)
 
-# Create a time series object for the ARIMA model with 5-minute intervals
-ts_data <- ts(data$`X_value`, frequency = 288)  # 288 intervals per day (5-minute intervals)
+# Create a time series object for the ARIMA model with 1-minute intervals
+ts_data <- ts(data$`X_value`, frequency = 1440)  # 1440 intervals per day (1-minute intervals)
 
 # Fit the ARIMA model
 arima_model <- auto.arima(ts_data)
 
 # Function to predict the price for a given date and time
 predict_price <- function(datetime) {
-  datetime <- as.POSIXct(datetime, format="%Y-%m-%d %H:%M:%S")
+  datetime <- as.POSIXct(datetime, format="%Y-%m-%d %H:%M:%S", tz="UTC")
   
-  # Calculate the number of 5-minute intervals from the last date-time in the dataset to the target date-time
+  # Calculate the number of 1-minute intervals from the last date-time in the dataset to the target date-time
   last_datetime <- max(data$createdDate)
-  intervals_to_predict <- as.numeric(difftime(datetime, last_datetime, units = "mins")) / 5
+  intervals_to_predict <- as.numeric(difftime(datetime, last_datetime, units = "mins"))  
   
   # Forecast the price
   forecast_result <- forecast(arima_model, h = intervals_to_predict)
